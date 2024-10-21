24f024728be7661ec598c863417c4ba644ce922e
Tianna <nguy4547@umn.edu>
2024-10-21 17:38:51 -0500

    We did things better!

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue + Tianna" />
+  <HelloWorld msg="Vite + Vue + Tianna + Nik too!" />
 </template>
 
 <style scoped>
